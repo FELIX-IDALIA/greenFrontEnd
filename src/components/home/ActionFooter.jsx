577073cb4af45a28e0956fc9e30d290a8332ee85
Felix Idalia <flxidalia@gmail.com>
2025-01-16 13:04:57 +0300

    Changed the tailwind CSS style

--- src/components/home/ActionFooter.jsx
+++ src/components/home/ActionFooter.jsx
@@ -2,7 +2,7 @@ import { Video, History, } from "lucide-react";
 import UnderDevPop from "../others/UnderDevPop.jsx";
 
 // Action Footer
-const ActionFooter = ({ handleButtonClick, showPopup, isSidebarOpen }) => {
+const ActionFooter = ({ handleButtonClick, showPopup, isSidebarOpen, handleGoLive }) => {
     return (
         <>
             <div className={`fixed bottom-0 left-0 w-full bg-white border-t border-gray-200 p-4 flex flex-wrap justify-center gap-4 transition-transform ${
@@ -20,7 +20,10 @@ const ActionFooter = ({ handleButtonClick, showPopup, isSidebarOpen }) => {
                     Go Live
                 </button>
                 {/*View Past Live Button*/}
-                <button className="flex items-center gap-2 bg-gray-100 text-gray-700 px-6 py-3 rounded-lg hover:bg-gray-300 transition">
+                <button 
+                    className="flex items-center gap-2 bg-gray-100 text-gray-700 px-6 py-3 rounded-lg hover:bg-gray-300 transition"
+                    onClick={handleGoLive}
+                >
                     <History className="h-5 w-5" />
                     View Past Live
                 </button>
