f135b3229fcf323d1ec24234aaa713e7b89f9242
Felix Idalia <flxidalia@gmail.com>
2025-01-17 13:34:56 +0300

    Extended tailwind's utility classes with custom CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blink: "blink 1.5s infinite", // Create a 'blink' animation with a 1.5s duration
+      },
+      keyframes: {
+        blink: {
+          "0%, 100%": {
+            opacity: "1", // Fully visible
+          },
+          "50%": {
+            opacity: "0", // Invisible halfway through the animation
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
