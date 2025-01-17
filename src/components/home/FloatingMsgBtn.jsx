9ca636ae227462b4b502aa3794b11d3c36a6c9fa
Felix Idalia <flxidalia@gmail.com>
2025-01-17 13:28:04 +0300

    Changed className -righ-2 to -right-2

--- src/components/home/FloatingMsgBtn.jsx
+++ src/components/home/FloatingMsgBtn.jsx
@@ -6,7 +6,7 @@ const FloatingMsgBtn = ({ toggleMessages, newMessages }) => {
         <div className="fixed bottom-20 right-4">
                 {/* Notification Badge */}
                 {newMessages > 0 && (
-                    <div className="absolute -top-2 -righ-2 bg-red-500 text-white text-xs font-bold w-5 h-5 rounded-full flex items-center justify-center shadow-md">
+                    <div className="absolute -top-2 -righ-2 bg-red-600 text-white text-xs font-bold w-5 h-5 rounded-full flex items-center justify-center shadow-md">
                         {newMessages > 99 ? "99+" : newMessages}
                     </div>
                 )}
